7c5e8c8184bf4f7c894d57d9318779c851aaace1
rahbirul <rahbirul.rubayeet@shopify.com>
2021-05-02 17:39:24 -0400

    Add Node server to serve static files and proxy requests to ES

---
+++ app/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const DIST = path.join(path.dirname(__dirname), 'node_modules');
+const STATICS = ['scripts', 'styles', 'images', 'views'];
+
+ const elasticsearchUrl = function () {
+   return process.env.ELASTICSEARCH_URL || 'http://quepid-elasticsearch.dev.o19s.com:9000';
+ }
+
+const proxyServer = createProxyMiddleware('/es_proxy', { 
+  target: elasticsearchUrl(), 
+  pathRewrite: {'^/es_proxy' : ''} 
+});
+
+const renderIndex = function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+}
+
+const app = express();
+
+// Define statics
+app.use('/node_modules', express.static(DIST));
+STATICS.forEach(function(static) {
+  app.use(`/${static}`, express.static(path.join(__dirname, static)) )
+});
+
+// Define proxy
+app.use('/es_proxy', proxyServer);
+
+// Render index
+app.get('/', renderIndex);
+
+app.listen(9000, () => {
+  console.log("Starting server at port 9000");
+});
